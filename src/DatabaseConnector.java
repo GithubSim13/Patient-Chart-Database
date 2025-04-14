7d59425500456d1fc1c3b826a4ea18f0051c8ca7
Jose Simbillo <144484430+GithubSim13@users.noreply.github.com>
2025-04-15 00:11:26 +0800

    Created patient DB on MySQL and connected it to code

---
+++ src/DatabaseConnector.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    //private static final String URL = "jdbc:mysql://localhost:3306/patients";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/patients";
+    private static final String USER = "root"; // or your MySQL username
+    private static final String PASSWORD = "Is!ma2friend3";
+
+    public static Connection connect() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.sql.*;
 
 public class SearchEngine {
     private ArrayList<Patient> patientList;
@@ -25,22 +26,44 @@ public class SearchEngine {
         System.out.print("Enter patient name : ");
         String name = sc.next();
 
-        boolean found = false;
+        searchPatientByLastName(name);
 
-        while (!found) {
-            for (int i = 0; i < patientList.size(); i++) {
-                Patient patient = patientList.get(i);
-                if (name.equals(patient.getLastName())) {
-                    System.out.println("Patient found!");
-                    found = true;
-                    break;
-                }
+//        boolean found = false;
+//
+//        while (!found) {
+//            for (int i = 0; i < patientList.size(); i++) {
+//                Patient patient = patientList.get(i);
+//                if (name.equals(patient.getLastName())) {
+//                    System.out.println("Patient found!");
+//                    found = true;
+//                    break;
+//                }
+//            }
+//            if (!found) {
+//                System.out.println("Patient not in database.");
+//                found = true;
+//            }
+//        }
+    }
+
+    public void searchPatientByLastName(String lastName) {
+        try (Connection conn = DatabaseConnector.connect()) {
+            String sql = "SELECT * FROM patients WHERE last_name = ?";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, lastName);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                System.out.println("Patient found:");
+                System.out.println(rs.getString("last_name") + ", " + rs.getString("first_name") + " " + rs.getString("middle_name"));
+                // You can print other details as well
+            } else {
+                System.out.println("Patient not found.");
             }
-            if (!found) {
-                System.out.println("Patient not in database.");
-                found = true;
-            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
     }
 }
